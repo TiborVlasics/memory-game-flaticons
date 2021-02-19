6ab7ec64d56dd514019334cfd27145fc62340357
tibi_vlasics <vlasics.tibor@nexius.hu>
2021-02-19 21:37:15 +0100

    Add flip animation on hover

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,12 @@
 function Card(props) {
   return (
-    <div>
-       <img src={props.card.src} alt="moray"/>
+    <div className="flip">
+      <div className="front">
+        <h2>Choose 2 cards</h2>
+      </div>
+      <div className="back">
+       <img src={props.card.src} alt={props.card.src} />
+      </div>
     </div>
   )
 }
